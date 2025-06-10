9e8fe6e9a0fa089ab1b13862bab737ea525eadfb
enzomarcondes <enzospmarcondes@gmail.com>
2025-06-10 11:45:13 -0400

    erro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push("/authentification");
-  };
-
-  return (
-    <main className="flex h-screen items-center justify-center">
-      <button
-        onClick={handleClick}
-        className="rounded-xl bg-blue-600 px-6 py-3 text-white transition hover:bg-blue-700"
-      >
-        Ir para Login
-      </button>
-    </main>
-  );
+  redirect("/authentication");
 }
